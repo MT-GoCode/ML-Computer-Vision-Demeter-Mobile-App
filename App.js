93221adfa098e6c0f1c3020f2dff86598c27bf2c
Minh Trinh <tminh.us@gmail.com>
2020-02-22 19:24:42 -0800

    Implement Upload and Take Photo

--- App.js
+++ App.js
@@ -1,89 +1,89 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
-import { Camera } from 'expo-camera';
-
-export default function App() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
-
-  useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
-
-  if (hasPermission === null) {
-    return <View />;
-  }
-  
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
-  return (
-    <View style={{ flex: 1 }}>
-      <Camera style={{ flex: 1 }} type={type}>
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: 'transparent',
-            flexDirection: 'row',
-          }}>
-          <TouchableOpacity
-            style={{
-              flex: 0.1,
-              alignSelf: 'flex-end',
-              alignItems: 'center',
-            }}
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
-            }}>
-            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
-    </View>
-  );
-}
-// import React, {useState} from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-// import SelectScreen from './components/SelectScreen'
-// import ImageInput from './components/ImageInput'
-// import CameraRollPicker from 'react-native-camera-roll-picker';
-
-// // import CameraRoll from './components/CameraRoll'
-// // import { RNCamera } from 'react-native-camera';
-// // import { Camera } from 'expo-camera';
+// import React, { useState, useEffect } from 'react';
+// import { Text, View, TouchableOpacity } from 'react-native';
+// import { Camera } from 'expo-camera';
 
 // export default function App() {
-//   const [selectedModel,setSelectedModel] = useState(null)
 //   const [hasPermission, setHasPermission] = useState(null);
-//   // const [type, setType] = useState(Camera.Constants.Type.back);
+//   const [type, setType] = useState(Camera.Constants.Type.back);
 
+//   useEffect(() => {
+//     (async () => {
+//       const { status } = await Camera.requestPermissionsAsync();
+//       setHasPermission(status === 'granted');
+//     })();
+//   }, []);
 
+//   if (hasPermission === null) {
+//     return <View />;
+//   }
+  
+//   if (hasPermission === false) {
+//     return <Text>No access to camera</Text>;
+//   }
 //   return (
-    
-//     <View style={styles.container} justifyContent = 'center'>
-//       {selectedModel}
-//       {/* <SelectScreen setSelectedModel = {setSelectedModel}/> */}
-//       <ImageInput/>
-//       {/* <CameraRollPicker
-//   callback={this.getSelectedImages} /> */}
-      
-      
+//     <View style={{ flex: 1 }}>
+//       <Camera style={{ flex: 1 }} type={type}>
+//         <View
+//           style={{
+//             flex: 1,
+//             backgroundColor: 'transparent',
+//             flexDirection: 'row',
+//           }}>
+//           <TouchableOpacity
+//             style={{
+//               flex: 0.1,
+//               alignSelf: 'flex-end',
+//               alignItems: 'center',
+//             }}
+//             onPress={() => {
+//               setType(
+//                 type === Camera.Constants.Type.back
+//                   ? Camera.Constants.Type.front
+//                   : Camera.Constants.Type.back
+//               );
+//             }}>
+//             <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+//           </TouchableOpacity>
+//         </View>
+//       </Camera>
 //     </View>
 //   );
 // }
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SelectScreen from './components/SelectScreen'
+import ImageInput from './components/ImageInput'
+import CameraRollPicker from 'react-native-camera-roll-picker';
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     // justifyContent: 'center',
-//   },
-// });
+// import CameraRoll from './components/CameraRoll'
+// import { RNCamera } from 'react-native-camera';
+// import { Camera } from 'expo-camera';
+
+export default function App() {
+  const [selectedModel,setSelectedModel] = useState(null)
+  const [hasPermission, setHasPermission] = useState(null);
+  // const [type, setType] = useState(Camera.Constants.Type.back);
+
+  
+
+  return (
+    
+    <View style={styles.container} justifyContent = 'center'>
+      {selectedModel}
+      <ImageInput/>
+      
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    // justifyContent: 'center',
+  },
+});
--- components/ImageInput.js
+++ components/ImageInput.js
@@ -1,16 +1,53 @@
-import React, { useState } from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View, Picker, FlatList,Image,Button, TouchableHighlight } from 'react-native';
-
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
 
 export default function ImageInput(props) {
-    return (
-        <View>
+
+  const [uri, setUri] = useState(null);
+  const CONFIG = {
+		allowsEditing: true
+  };
+  
+  useEffect(() => {
+    (
+      async () => {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA);
+      // setHasPermission(status === 'granted');
+      
+    }
+  )();
+  (
+    async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    // setHasPermission(status === 'granted');
+    
+  }
+  )();
+  }, []);
+
+  const openCamera = async () => { const resp = await ImagePicker.launchCameraAsync(CONFIG);
+    if (resp) {
+      console.log(resp.uri);
+        setUri(resp.uri);
+    }
+  };
+
+  const openCameraRoll = async () => {
+    const resp = await ImagePicker.launchImageLibraryAsync(CONFIG);
+    if (resp)
+      setUri(resp.uri);
+  };
+
+  return (
+      <View>
         <Image style  = {{width:65, height: 65, margin: 50, padding: 50}} source = {require('../assets/tomatoicon.png')}/>
         <Text>Tomato</Text>
         <TouchableHighlight
             // title="Upload Image"
             style = {styles.button}
-              onPress={() => {}}
+              onPress={openCameraRoll}
         >
             <Text>Upload Image</Text>
         </TouchableHighlight>
@@ -18,14 +55,17 @@ export default function ImageInput(props) {
         <TouchableHighlight
             // title="Upload Image"
             style = {styles.button}
-              onPress={() => {}}
+              onPress={openCamera}
         >
             <Text>Take Photo</Text>
         </TouchableHighlight>
         
-        </View>
-      );
+      </View>
+    );
 }
+
+
+
 
 const styles = StyleSheet.create({
     container: {
